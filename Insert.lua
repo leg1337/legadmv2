19e39425ef874c0707668ebb4a7881a2e0ecaa7a
leg1337 <88927991+leg1337@users.noreply.github.com>
2022-08-28 20:26:25 +0300

    Update Insert.lua

--- Insert.lua
+++ Insert.lua
@@ -1,6 +1,5 @@
 local inv = "https://discord.gg/8RppsqJuFm"
 setclipboard(tostring(inv))
-
 function notify(x, y, t)
     game.StarterGui:SetCore(
         "SendNotification",
@@ -12,8 +11,18 @@ function notify(x, y, t)
     )
 end
 notify("set inv to clipboard", "leg's admin v2", 3)
+
 task.wait(2)
+
 notify("joining discord...", "leg's admin v2", 1")
-syn.request({Url = "http://127.0.0.1:6463/rpc?v=1", Method = "POST", Headers = { ["Content-Type"] = "application/json", ["Origin"] = "https://discord.com" },
-Body = game:GetService("HttpService"):JSONEncode({cmd = "INVITE_BROWSER", args = {code = "8RppsqJuFm" }, nonce = game:GetService("HttpService"):GenerateGUID(false)})})
+ 
+syn.request({Url = "http://127.0.0.1:6463/rpc?v=1",
+            
+Method = "POST", Headers = { ["Content-Type"] = "application/json",
+                
+["Origin"] = "https://discord.com" },
+            
+Body = game:GetService("HttpService"):JSONEncode({cmd = "INVITE_BROWSER",
+                    
+args = {code = "8RppsqJuFm" }, nonce = game:GetService("HttpService"):GenerateGUID(false)})})
 
